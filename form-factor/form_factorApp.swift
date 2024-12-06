79e51077f728062af9389a538964fcc1ef0b9299
FC <drslen9@gmail.com>
2024-12-06 01:37:37 -0500

    Initial Commit

---
+++ form-factor/form_factorApp.swift
@@ -0,0 +1,17 @@
+//
+//  form_factorApp.swift
+//  form-factor
+//
+//  Created by Fabrizio Corrales on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct form_factorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ form-factorTests/form_factorTests.swift
@@ -0,0 +1,17 @@
+//
+//  form_factorTests.swift
+//  form-factorTests
+//
+//  Created by Fabrizio Corrales on 12/6/24.
+//
+
+import Testing
+@testable import form_factor
+
+struct form_factorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
