7294e05b5b67689cb2d812e9b21cca19c504d9fe
FC <drslen9@gmail.com>
2025-09-29 19:45:30 -0700

    docs: Add pose detection setup documentation and native files

---
+++ native/VisionPoseDetector.m
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+#import <VisionCamera/FrameProcessorPlugin.h>
+#import <VisionCamera/FrameProcessorPluginRegistry.h>
+#import "formfactoreas-Swift.h"
+
+@interface VisionPoseDetectorPlugin : FrameProcessorPlugin
+@end
+
+@implementation VisionPoseDetectorPlugin
+
+VISION_EXPORT_SWIFT_FRAME_PROCESSOR(VisionPoseDetectorPlugin, detectPose)
+
+- (id)callback:(Frame *)frame withArguments:(NSDictionary *)arguments {
+  return [VisionPoseDetector callbackWithFrame:frame withArguments:arguments];
+}
+
+@end
