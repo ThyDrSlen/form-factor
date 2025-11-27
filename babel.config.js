f542b36675eb7fe290f683c102c4e4f4dfe8780b
FC <drslen9@gmail.com>
2025-11-27 09:44:23 -0500

    chore(tooling): add Jest test infrastructure
    - Touches: jest.config.js, babel.config.js, package.json
    - Outcome: Jest + Testing Library setup with jest-expo preset
    - Notes: Babel disables module-resolver in test env for Jest moduleNameMapper

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,24 @@
 module.exports = function (api) {
-  api.cache(true);
+  const isTest = api.env('test');
+  api.cache(() => isTest);
+
+  const plugins = [
+    ['@babel/plugin-transform-runtime', {
+      helpers: true,
+      regenerator: true,
+    }],
+    // Disable module-resolver in tests so Jest's moduleNameMapper can handle path aliases
+    !isTest && ['module-resolver', {
+      root: ['./'],
+      alias: {
+        '@': './',
+      },
+    }],
+    'react-native-reanimated/plugin',
+  ].filter(Boolean);
+
   return {
     presets: ['babel-preset-expo', 'nativewind/babel'],
-    plugins: [
-      ['@babel/plugin-transform-runtime', {
-        helpers: true,
-        regenerator: true,
-      }],
-      ['module-resolver', {
-        root: ['./'],
-        alias: {
-          '@': './',
-        },
-      }],
-      'react-native-reanimated/plugin',
-    ],
+    plugins,
   };
 };
