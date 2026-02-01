4e91de6d6b0c102d8fc77a711508b1035e1b97f3
FC <drslen9@gmail.com>
2026-01-31 16:52:07 -0800

    fix(watch): set watchOS deployment target
    - Touches: watch target config
    
    - Outcome: aligns watch build target with Xcode 26.1
    
    - Notes: None

--- targets/watch-app/expo-target.config.js
+++ targets/watch-app/expo-target.config.js
@@ -2,6 +2,7 @@ module.exports = {
   type: 'watch',
   name: 'Form Factor Watch Watch App',
   displayName: 'Form Factor',
+  deploymentTarget: '26.1',
   bundleIdentifier: 'com.slenthekid.formfactoreas.watchkitapp',
   entitlements: {
     'com.apple.developer.healthkit': true,
